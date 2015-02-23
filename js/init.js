31e71e08369c8b712279df35501f9acfbe4ce242
Bryan Woolbright <woolyman84@gmail.com>
2015-02-23 16:51:09 -0600

    Add function to show/hide sections on homepage

--- js/init.js
+++ js/init.js
@@ -5,16 +5,24 @@ $(document).ready(function() {
 function setupBackgroundImage(){
   $('body').removeClass();
   $('body').addClass('about');
+  $('section').hide();
+  $('section.about').show(300);
   $('#work_nav').click(function(){
     $('body').removeClass();
     $('body').addClass('work');
+    $('section').hide();
+    $('section.work').show(300);
   });
   $('#contact_nav').click(function(){
     $('body').removeClass();
     $('body').addClass('contact');
+    $('section').hide();
+    $('section.contact').show(300);
   });
   $('#about_nav').click(function(){
     $('body').removeClass();
     $('body').addClass('about');
+    $('section').hide();
+    $('section.about').show(300);
   });
 }
