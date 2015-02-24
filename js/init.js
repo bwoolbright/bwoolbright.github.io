beafc86c65a1f2ebf12814fb2f2e12acc08d5e72
Bryan Woolbright <woolyman84@gmail.com>
2015-02-23 21:14:23 -0600

    Add functionality

--- js/init.js
+++ js/init.js
@@ -5,27 +5,23 @@ $(document).ready(function() {
 function setupBackgroundImage(){
   $('body').removeClass();
   $('body').addClass('about');
-  $('body').css('background-image').fadeIn(300);
   $('section').hide();
   $('section.about').show(300);
   $('#work_nav').click(function(){
     $('body').removeClass();
     $('body').addClass('work');
-    $('body').css('background-image').fadeIn(300);
     $('section').hide();
     $('section.work').show(300);
   });
   $('#contact_nav').click(function(){
     $('body').removeClass();
     $('body').addClass('contact');
-    $('body').css('background-image').fadeIn(300);
     $('section').hide();
     $('section.contact').show(300);
   });
   $('#about_nav').click(function(){
     $('body').removeClass();
     $('body').addClass('about');
-    $('body').css('background-image').fadeIn(300);
     $('section').hide();
     $('section.about').show(300);
   });
