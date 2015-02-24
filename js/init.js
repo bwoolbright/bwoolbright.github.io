87d011c2064e55c6416d20c7464c07c63e4feafe
Bryan Woolbright <woolyman84@gmail.com>
2015-02-23 21:31:10 -0600

    Add animsition function in ini js file

--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   setupBackgroundImage();
+  setupAnimsition();
 });
 
 function setupBackgroundImage(){
@@ -26,3 +27,29 @@ function setupBackgroundImage(){
     $('section.about').show(300);
   });
 }
+
+function setupAnimsition(){
+  $(".animsition").animsition({
+    
+      inClass               :   'fade-in',
+      outClass              :   'fade-out',
+      inDuration            :    1500,
+      outDuration           :    800,
+      linkElement           :   '.animsition-link',
+      // e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
+      loading               :    true,
+      loadingParentElement  :   'body', //animsition wrapper element
+      loadingClass          :   'animsition-loading',
+      unSupportCss          : [ 'animation-duration',
+                                '-webkit-animation-duration',
+                                '-o-animation-duration'
+                              ],
+      //"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+      //The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+      
+      overlay               :   false,
+      
+      overlayClass          :   'animsition-overlay-slide',
+      overlayParentElement  :   'body'
+    });
+  }
